2c284b4ca8cd40c626ce1ef4b9b15c555386bea8
Peter John Alvarado <redjoker011@gmail.com>
2024-09-20 19:00:32 +0800

    feat: update nvim tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,29 +1,33 @@
-return { 
-  "kyazdani42/nvim-tree.lua", 
-  opts = { 
-    sort_by = "case_sensitive",
-    hijack_cursor = true,
-    view = {
-      width = 30,
-      side = 'right',
-    },
-    actions = {
-      open_file = {
-        window_picker = {
-          enable = false
+return {
+  "kyazdani42/nvim-tree.lua",
+  config = function()
+    require("nvim-tree").setup({
+      sort_by = "case_sensitive",
+      hijack_cursor = true,
+      view = {
+        width = 30,
+        side = 'right',
+      },
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false
+          }
         }
-      }
-    },
-    renderer = {
-      group_empty = true,
-    },
-    filters = {
-      dotfiles = true,
-    },
-    diagnostics = {
-      enable = true,
-      show_on_dirs = true,
-      show_on_open_dirs = true,
-    },
-  }
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+      },
+    })
+    vim.keymap.set("n", "<Leader>v", ":NvimTreeToggle<CR>", { silent = true })
+    vim.keymap.set("n", "<Leader>t", ":NvimTreeFocus<CR>", { silent = true })
+  end
 }
