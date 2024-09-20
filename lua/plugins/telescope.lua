22cea8a6ac78baf281c819c5e23f5bbbf25fbffd
Peter John Alvarado <redjoker011@gmail.com>
2024-09-20 20:56:52 +0800

    feat: update telescope use fzf_native plugin as dependency

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,69 +1,81 @@
- return {
-    "nvim-telescope/telescope.nvim",
-    lazy = true,
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>sf",
-        function() 
-          require("telescope.builtin").find_files()
-        end,
-        desc = "Find Plugin File",
+return {
+  "nvim-telescope/telescope.nvim",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release" }
+  },
+  keys = {
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<leader>sf",
+      function()
+        require("telescope.builtin").find_files()
+      end,
+      desc = "Find Plugin File",
+    },
+    {
+      "<leader><space>",
+      function()
+        require("telescope.builtin").buffers()
+      end,
+      desc = "Find existing buffers",
+    },
+    {
+      "<leader>sg",
+      function()
+        require("telescope.builtin").live_grep()
+      end,
+      desc = "Search by Grep",
+    },
+    {
+      "<leader>sw",
+      function()
+        require("telescope.builtin").search_string()
+      end,
+      desc = "Search current Word",
+    },
+    {
+      "<leader>sd",
+      function()
+        require("telescope.builtin").diagnostics()
+      end,
+      desc = "Search Diagnostics",
+    },
+  },
+  -- change some options
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--trim" -- add this value
       },
-      {
-        "<leader><space>",
-        function() 
-          require("telescope.builtin").buffers()
-        end,
-        desc = "Find existing buffers",
-      },
-      {
-        "<leader>sg",
-        function() 
-          require("telescope.builtin").live_grep()
-        end,
-        desc = "Search by Grep",
-      },
-      {
-        "<leader>sw",
-        function() 
-          require("telescope.builtin").search_string()
-        end,
-        desc = "Search current Word",
-      },
-      {
-        "<leader>sd",
-        function() 
-          require("telescope.builtin").diagnostics()
-        end,
-        desc = "Search Diagnostics",
+      mappings = {
+        i = {
+          ['<C-u>'] = false,
+          ['<C-d>'] = false,
+        },
       },
     },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-        vimgrep_arguments = {
-          "rg",
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case",
-          "--trim" -- add this value
-        },
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
-          },
-        },
-      },
-    },
-  }
+    extensions = {
+      fzf = {
+        fuzzy = true,                   -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true,    -- override the file sorter
+        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      }
+    }
+  },
+}
