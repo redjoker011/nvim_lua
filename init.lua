07a9855df1e8f2103b9cb48c61ccea9dad800ab7
Peter John Alvarado <redjoker011@gmail.com>
2024-09-20 19:48:29 +0800

    feat: add yank on highlight command

--- init.lua
+++ init.lua
@@ -83,6 +83,21 @@ vim.opt.termguicolors = true
 vim.keymap.set('i', 'jk', '<esc>')
 vim.keymap.set('i', '<esc>', '<nop>')
 
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
 -- Enforce using j and k when moving
 vim.keymap.set('n', '<Up>', function() print 'Use k' end)
 vim.keymap.set('n', '<Left>', function() print 'Use h' end)
