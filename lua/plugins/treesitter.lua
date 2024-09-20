449269b34f0ecdee37999465edff01f8b8e27c13
Peter John Alvarado <redjoker011@gmail.com>
2024-09-20 17:49:42 +0800

    feat: add treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "regex",
+      "typescript",
+      "vim",
+      "yaml",
+      'go',
+      'vim',
+      'ruby',
+      'sql',
+      'css',
+      'scss'
+    },
+  },
+}
