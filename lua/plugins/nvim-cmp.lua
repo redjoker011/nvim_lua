05b4288328dbaa888dbe933cb9468edae284fc51
Peter John Alvarado <redjoker011@gmail.com>
2025-05-29 22:09:33 +0800

    feat: add hardtime vim plugin

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,7 +2,10 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     { "hrsh7th/cmp-nvim-lsp" },
-    { "L3MON4D3/LuaSnip" },
+    {
+      "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp"
+    },
     { "rafamadriz/friendly-snippets" },
     { "saadparwaiz1/cmp_luasnip" },
     { "hrsh7th/cmp-buffer" },
