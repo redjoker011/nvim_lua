49e9ce8842b337fc07017f2362a29e13ebe312bf
pete_jdv <p.alvarado@jvd.com.ph>
2025-09-14 15:36:59 +0800

    feat: add plugin grug-far add plugin for faster search and replace

---
+++ lua/plugins/grug-far.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "MagicDuck/grug-far.nvim",
+    cmd = "GrugFar",
+    opts = {
+      headerMaxWidth = 80,
+    },
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.open({
+            transient = true,
+            prefills = {
+              filesFilter = (ext and ext ~= "" and "*." .. ext) or nil,
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
+  },
+}
