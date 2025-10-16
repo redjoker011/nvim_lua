58b75647b7af49e8ddadd6a609d0330d6af93daf
Peter John Alvarado <redjoker011@gmail.com>
2025-10-17 00:46:01 +0800

    feat: add vim-snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,40 +2,51 @@ return {
   "hrsh7th/nvim-cmp",
   dependencies = {
     { "hrsh7th/cmp-nvim-lsp" },
-    {
-      "L3MON4D3/LuaSnip",
-      build = "make install_jsregexp"
-    },
-    { "rafamadriz/friendly-snippets" },
     { "saadparwaiz1/cmp_luasnip" },
     { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" }
+    { "hrsh7th/cmp-path" },
+    {
+      "L3MON4D3/LuaSnip",
+      build = "make install_jsregexp",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+        "honza/vim-snippets", -- Add this for full Rails snippets
+      },
+      config = function()
+        local luasnip = require("luasnip")
+
+        -- Load snippets from multiple sources
+        require("luasnip.loaders.from_vscode").lazy_load()   -- friendly-snippets
+        require("luasnip.loaders.from_snipmate").lazy_load() -- vim-snippets
+
+        -- Optional: load your own snippets from Lua
+        -- require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
+
+        -- Extend Ruby snippets to include Rails
+        luasnip.filetype_extend("ruby", { "rails" })
+      end,
+    },
   },
+
   config = function()
-    -- lazy load snippets from vscode source via rafamadriz/friendly-snippets
-    require("luasnip.loaders.from_vscode").lazy_load()
-    -- snippet extensions
-    require 'luasnip'.filetype_extend("ruby", { "rails" })
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-    -- nvim-cmp setup
-    local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-
-    cmp.setup {
+    cmp.setup({
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
-      mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm {
+      mapping = cmp.mapping.preset.insert({
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -43,8 +54,8 @@ return {
           else
             fallback()
           end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
@@ -52,29 +63,30 @@ return {
           else
             fallback()
           end
-        end, { 'i', 's' }),
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "path" },
         { name = "buffer" },
-      },
+      }),
       window = {
-        documentation = cmp.config.window.bordered()
+        documentation = cmp.config.window.bordered(),
       },
       formatting = {
-        fields = { 'menu', 'abbr', 'kind' },
+        fields = { "menu", "abbr", "kind" },
         format = function(entry, item)
           local menu_icon = {
-            nvim_lsp = 'λ',
-            luasnip = '⋗',
+            nvim_lsp = "λ",
+            luasnip = "⋗",
+            buffer = "Ω",
+            path = "🖫",
           }
-
           item.menu = menu_icon[entry.source.name]
           return item
-        end
+        end,
       },
-    }
-  end
+    })
+  end,
 }
