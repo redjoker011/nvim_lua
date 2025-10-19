4fd02af5dd502b56cbe8f59e724345bdec3b8859
pete_jdv <p.alvarado@jvd.com.ph>
2025-10-19 22:49:40 +0800

    feat: add key mappings for vim-fugitive to improve workflow

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,36 @@
 return {
-  "tpope/vim-fugitive"
+  "tpope/vim-fugitive",
+  cmd = {
+    "Git", "G", "Gblame", "Gdiffsplit", "Gedit", "Gsplit", "Gtabedit",
+    "Gread", "Gwrite", "Ggrep", "Gmove", "Gdelete",
+  },
+  keys = {
+    { "<leader>gs", "<cmd>Git<CR>",                                             desc = "Git (run git command)",                           mode = "n" },
+    { "<leader>gS", "<cmd>Git status<CR>",                                      desc = "Git status (explicit)",                           mode = "n" },
+    { "<leader>gb", "<cmd>G blame<CR>",                                         desc = "Git blame (interactive split)",                   mode = "n" },
+    { "<leader>gd", "<cmd>Gdiffsplit<CR>",                                      desc = "Git diff (vertical split)",                       mode = "n" },
+    { "<leader>gD", "<cmd>Gdiffsplit!<CR>",                                     desc = "Git diff (horizontal split)",                     mode = "n" },
+    { "<leader>ge", "<cmd>Gedit<CR>",                                           desc = "Open file/commit under cursor (Gedit)",           mode = "n" },
+    { "<leader>gE", "<cmd>Gtabedit<CR>",                                        desc = "Open file/commit under cursor in new tab",        mode = "n" },
+    { "<leader>gC", "<cmd>Git commit<CR>",                                      desc = "Git commit",                                      mode = "n" },
+    { "<leader>gp", "<cmd>Git push<CR>",                                        desc = "Git push",                                        mode = "n" },
+    { "<leader>gP", "<cmd>Git pull<CR>",                                        desc = "Git pull",                                        mode = "n" },
+    { "<leader>gr", "<cmd>Gread<CR>",                                           desc = "Revert buffer to HEAD (discard working changes)", mode = "n" },
+    { "<leader>gw", "<cmd>Gwrite<CR>",                                          desc = "Stage current file (write to index)",             mode = "n" },
+    { "<leader>gl", "<cmd>Git --no-pager log --oneline --graph --decorate<CR>", desc = "Git log (graph)",                                 mode = "n" },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "fugitive", "fugitiveblame" },
+      callback = function(event)
+        local opts = { buffer = event.buf, silent = true }
+        -- q to quit fugitive/blame buffers
+        vim.keymap.set("n", "q", "<cmd>q<CR>", opts)
+        -- Enter to open the commit / blob under the cursor (Gedit)
+        vim.keymap.set("n", "<CR>", "<cmd><C-u>Gedit<CR>", vim.tbl_extend("force", opts, { desc = "Open commit/blob" }))
+        -- Helpful: close blame and return to working buffer with gq (behaves like Fugitive's default)
+        vim.keymap.set("n", "gq", "<cmd>Gedit<CR>", opts)
+      end,
+    })
+  end,
 }
