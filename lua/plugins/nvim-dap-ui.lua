f721a390291256cc0406c0d6ea9fc38259105bbd
Peter John Alvarado <redjoker011@gmail.com>
2026-04-07 17:58:29 +0800

    feat: add keymappings to improve interaction with nvim-dap

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -5,6 +5,24 @@ return {
     local dapui = require("dapui")
     dapui.setup()
 
+    local function float(element)
+      local width = math.floor(vim.o.columns * 0.85)
+      local height = math.floor(vim.o.lines * 0.85 - 2) -- subtract cmdline/status space
+
+      dapui.float_element(element, {
+        width = width,
+        height = height,
+        position = "center",
+      })
+      vim.cmd("wincmd p")
+    end
+
     vim.keymap.set("n", "<Leader>du", dapui.toggle)
+
+    -- Zoom-specific panels
+    vim.keymap.set("n", "<leader>dr", function()
+      float("repl")
+      vim.cmd("startinsert") -- go directly into insert mode
+    end, { desc = "Zoom DAP REPL" })
   end,
 }
