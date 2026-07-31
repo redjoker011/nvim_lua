4ea524c5f791dd72d1399dc3afc5807ffd46f11d
Peter John Alvarado <redjoker011@gmail.com>
2026-07-31 20:57:10 +0800

    feat: update dracula theme plugin source

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,64 +1,23 @@
 return {
-  {
-    'maxmx03/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      ---@type dracula
-      local dracula = require "dracula"
-
-      dracula.setup({
-        styles = {
-          types = {},
-          functions = {},
-          parameters = {},
-          comments = {},
-          strings = {},
-          keywords = {},
-          variables = {},
-          constants = {},
-        },
-        transparent = false,
-        on_colors = function(colors, color)
-          ---@type dracula.palette
-          return {
-            -- override or create new colors
-            mycolor = "#ffffff",
-          }
-        end,
-        on_highlights = function(colors, color)
-          ---@type dracula.highlights
-          return {
-            ---@type vim.api.keyset.highlight
-            Normal = { fg = colors.mycolor }
-          }
-        end,
-        plugins = {
-          ["nvim-treesitter"] = true,
-          ["rainbow-delimiters"] = true,
-          ["nvim-lspconfig"] = true,
-          ["nvim-navic"] = true,
-          ["nvim-cmp"] = true,
-          ["indent-blankline.nvim"] = true,
-          ["neo-tree.nvim"] = true,
-          ["nvim-tree.lua"] = true,
-          ["which-key.nvim"] = true,
-          ["dashboard-nvim"] = true,
-          ["gitsigns.nvim"] = true,
-          ["neogit"] = true,
-          ["todo-comments.nvim"] = true,
-          ["lazy.nvim"] = true,
-          ["telescope.nvim"] = true,
-          ["noice.nvim"] = true,
-          ["hop.nvim"] = true,
-          ["mini.statusline"] = true,
-          ["mini.tabline"] = true,
-          ["mini.starter"] = true,
-          ["mini.cursorword"] = true,
-          ['bufferline.nvim'] = true,
-        }
-      })
-      vim.cmd.colorscheme 'dracula'
-    end
-  },
+  "Mofiqul/dracula.nvim",
+  lazy = false,    -- Load during startup if this is your primary colorscheme
+  priority = 1000, -- Load prior to other UI plugins
+  config = function()
+    local dracula = require("dracula")
+    dracula.setup({
+      -- Choose between "dracula" and "dracula-soft"
+      theme = "dracula",
+      -- Set to true to disable background colors (transparent terminal support)
+      transparent_bg = false,
+      -- Styling options
+      italic_comment = true,
+      show_end_of_buffer = true,
+      -- Custom palette or highlight overrides
+      colors = {
+        bg = "#282A36",
+        fg = "#F8F8F2",
+      },
+    })
+    vim.cmd.colorscheme("dracula")
+  end,
 }
